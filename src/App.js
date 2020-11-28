d29b0af0859475f13f765ccf46197d1af99f556c
pjmantoss <joelptoss@gmail.com>
2020-11-28 16:40:36 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 
 function App() {
-  const STARTING_TIME = 5;
+  const STARTING_TIME = 60;
   const [text, setText] = useState("");
   const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME);
   const [isTimeRunning, setIsTimeRunning] = useState(false);
