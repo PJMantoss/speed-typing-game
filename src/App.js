b746d2269fceab7a0d6b703b965b688fb239c244
pjmantoss <joelptoss@gmail.com>
2020-11-28 12:43:01 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <h1>How fast do you type?</h1>
       <textarea />
-      <h4>Time remaining</h4>
+      <h4>Time remaining: ???</h4>
       <button>Start</button>
       <h1>Word Count</h1>
     </div>
