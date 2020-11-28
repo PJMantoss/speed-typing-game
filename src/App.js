09c7fc05405c0bdcd1f8152e3ea2ffcab3e24776
pjmantoss <joelptoss@gmail.com>
2020-11-28 13:42:12 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   const [text, setText] = useState("");
-  const [timer, setTimer] = useState(0);
+  const [timeRemaining, settimeRemaining] = useState(0);
   
   const handleChange = (e) => {
     const {value} = e.target;
