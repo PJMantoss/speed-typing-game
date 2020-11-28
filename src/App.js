d7a17f0f53d1ed6b57299b1e8575dcb37aba573f
pjmantoss <joelptoss@gmail.com>
2020-11-28 08:49:52 +0100

    update App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1></h1>
+      <h1>Title</h1>
       <textarea />
-      <h4></h4>
-      <button></button>
-      <h1></h1>
+      <h4>Time remaining</h4>
+      <button>Start</button>
+      <h1>Word Count</h1>
     </div>
   );
 }
